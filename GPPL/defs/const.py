cb9370095500661c17d0e809124dbdf2fc3a58d1
Ulisse Rubizzo <urubizzo@pdxeng.ch>
2018-12-28 14:56:38 +0100

    Added status/error types
    Added a status enumeration type with 3 different values
    Added an error enueration type with 4 different levels of errror

--- GPPL/defs/const.py
+++ GPPL/defs/const.py
@@ -4,5 +4,16 @@
 
 """
 
-from typing import Any, Union, Optional
+from enum import IntEnum
 
+class Status(IntEnum):
+    OK = 1
+    NOT_OK = 2
+
+    ERR = -1
+
+class ErrorCode(IntEnum):
+    NORMAL = -1
+    HIGH = -10    
+    CRITICAL = -100
+    BLOCKING = -1000
